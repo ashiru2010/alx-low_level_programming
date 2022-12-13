0f927dae88a6fd4990adc31686266447dcabc01b
ashiru2010 <ashirubalili@yahoo.com>
2022-12-13 08:17:51 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print a line of code using puts
+ * code by balili
+ * Return: 0
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
