fb679244e88ecf786a2698bc794cbbbf4774603f
ashiru2010 <ashirubalili@yahoo.com>
2022-12-15 16:09:14 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+}
