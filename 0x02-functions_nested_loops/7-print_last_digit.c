4489baee843442062a6269e3e55eff0cd148c41b
ashiru2010 <ashirubalili@yahoo.com>
2022-12-18 15:00:03 -0800

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,14 +11,12 @@ int print_last_digit(int n)
 
 	if (n < 0)
 	{
-		last_digit = (-n + (n % 10));
+		n = -n
+		last_digit = (n % 10);
 		_putchar(last_digit + '0');
-		return (last_digit);
 	}
 	else
 	{
-		last_digit = (n % 10);
-		_putchar(last_digit + '0');
 		return (last_digit);
 	}
 }
