4cb0edf2a770fd9c1abcb44ea85a7d93b350dfcf
ashiru2010 <ashirubalili@yahoo.com>
2022-12-18 01:52:14 -0800

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints lowercase alphabet in reverse
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while (c >= 'a')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
