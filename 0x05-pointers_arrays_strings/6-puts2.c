4678828a754dfc2b0e412539d19060e54a22b299
ashiru2010 <ashirubalili@yahoo.com>
2022-12-20 01:09:07 -0800

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,17 +7,16 @@
 
 void puts2(char *str)
 {
-	int count = 0;
+	int count;
 
-	while (count >= 0)
+	count = 0;
+	while (str[count] != '\0')
 	{
-		if (str[count] == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
 		if (count % 2 == 0)
+		{
 			_putchar(str[count]);
+		}
 		count++;
 	}
+	_putchar('\n');
 }
