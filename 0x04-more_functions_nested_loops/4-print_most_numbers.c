19701238f418e24daa08974c577684f32370d04a
ashiru2010 <ashirubalili@yahoo.com>
2022-12-20 21:48:56 -0800

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,12 @@
 
 void print_most_numbers(void)
 {
-	char c;
+	int num;
 
-	for (c >= '0'; c <= '9'; c++)
+	for (num = '0'; num <= '9'; num++)
 	{
-		if (!(c == '2' || c == '4'))
-			_putchar(c);
+		if (!(num == '2' || num == '4'))
+			_putchar(num);
 	}
 	_putchar('\n');
 }
