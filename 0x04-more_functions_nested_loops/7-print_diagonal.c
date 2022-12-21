0bae0174d4516f3ddbf79a8768803a2ac4a7a5cb
ashiru2010 <ashirubalili@yahoo.com>
2022-12-20 22:33:10 -0800

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_diagonal - prints diagonal line
+ * @n: print int
+ * Return: 0
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int b;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (b = 0; b < i; b++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+		}
+		_putchar('\n');
+	}
+}
