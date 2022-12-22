0989f47d0bf2e422d107cc45c6adc1a674fc3b1e
ashiru2010 <ashirubalili@yahoo.com>
2022-12-22 01:52:28 -0800

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,11 +13,11 @@ char *leet(char *str)
 
 	while (str[++index1])
 	{
-		for (index2 = 0; index2 <= 7; index2++)
+		for (index2 = 0; index2 <= 8; index2++)
 		{
 			if (str[index1] == leet[index2] || str[index1] - 32 == leet[index2])
 			{
-				str[index1] = index2 + '\0';
+				str[index1] = index2 + '0';
 			}
 		}
 	}
