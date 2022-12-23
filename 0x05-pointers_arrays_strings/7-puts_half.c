d01a06368a0b1a6d0e157b3d461bae3e5ee9b2f5
ashiru2010 <ashirubalili@yahoo.com>
2022-12-23 12:12:36 -0800

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ *
+ * @str: input string
+ */
+
+void puts_half(char *str)
+{
+	int index, half;
+
+	index = 0;
+	while (str[index] != '\0')
+		index++;
+
+	half = index / 2;
+
+	if (index % 2 == 1)
+		half++;
+
+	while (half < index)
+	{
+		_putchar(str[half]);
+		half++;
+	}
+	_putchar('\n');
+}
