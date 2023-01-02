953a4ad002995468f4932f48656d1f758411802d
ashiru2010 <ashirubalili@yahoo.com>
2023-01-02 12:06:55 -0800

    task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -22,7 +22,7 @@ void print_diagsums(int *a, int size)
 	{
 		sumDiag1 = sumDiag2 + a[i];
 	}
-	for (i = (size - 1); i <= size1; i = i + (size - 1))
+	for (i = (size - 1); i < size1; i = i + (size - 1))
 	{
 		sumDiag2 = sumDiag2 + a[i];
 	}
