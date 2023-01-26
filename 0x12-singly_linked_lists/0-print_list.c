7c01f933c11d071991434c23b5d95000d44c4d50
ashiru2010 <ashirubalili@yahoo.com>
2023-01-26 07:42:13 -0800

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,9 +7,9 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nol;
+	size_t nelem;
 
-	nol = 0;
+	nelem = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		nol++;
+		nelem++;
 	}
-	return (nol);
+	return (nelem);
 }
