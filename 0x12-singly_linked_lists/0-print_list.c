1f8dc19532abc328a8f190dfdc7858691222e35f
ashiru2010 <ashirubalili@yahoo.com>
2023-01-26 08:04:51 -0800

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,11 +14,15 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
+		{
 			printf("[%d] %s\n", 0, "(nil)");
+		}
 		else
+		{
 			printf("[%d] %s\n", h->len, h->str);
+		}
 		h = h->next;
-		nelem++;
 	}
+	nelem++;
 	return (nelem);
 }
