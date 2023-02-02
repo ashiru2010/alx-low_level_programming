e4fd0c7544493fffe1df0759de0a454d10194103
ashiru2010 <ashirubalili@yahoo.com>
2023-02-02 12:20:47 -0800

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+* print_binary - prints the binary representation of a number
+* @n: number to be converted
+* Return: a binary number
+*/
+void print_binary(unsigned long int n)
+{
+	unsigned long int i, counter = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	if (n == 1)
+	{
+		_putchar('1');
+		return;
+	}
+
+	i = 2;
+
+	while (i <= n && counter < 62)
+	{
+		i <<= 1;
+		counter++;
+	}
+
+	if (counter != 62)
+		i >>= 1;
+
+	while (i != 0)
+	{
+		if ((i & n) != 0)
+		{
+			counter++;
+			_putchar('1');
+		}
+		else
+		{
+			counter++;
+			_putchar('0');
+		}
+		i >>= 1;
+	}
+}
