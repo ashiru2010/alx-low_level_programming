b5418af4d68262208f340fc11f61077892384d43
ashiru2010 <ashirubalili@yahoo.com>
2023-02-02 12:35:50 -0800

    task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* clear_bit - sets the value of a bit to 0 at a given index
+* @n: number to convert
+* @index: index, starting from 0 of the bit you want to set
+* Return: 1 if it worked or -1 if an error occurred
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int mask;
+
+	if (index > 64)
+		return (-1);
+
+	mask = 1 << index;
+	*n &= ~mask;
+	return (1);
+}
