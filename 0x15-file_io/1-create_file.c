e186ae405dc9dad54475b11e3f3e4fb6fc3a751d
ashiru2010 <ashirubalili@yahoo.com>
2023-02-06 02:39:56 -0800

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_file - Function that creates a file and adds permission to it.
+ * @filename: Name of the file to be created
+ * @text_content: Content to be placed in the new file
+ * Return: 1 if successfull and -1 for err.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t fild = 0, chk = 0, i = 0;
+
+	if (!filename)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	fild = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00600);
+	if (fild < 0)
+		return (-1);
+
+	while (text_content[i])
+		i++;
+	chk = write(fild, text_content, i);
+	if (chk < 0)
+		return (-1);
+
+	close(fild);
+	return (1);
+}
