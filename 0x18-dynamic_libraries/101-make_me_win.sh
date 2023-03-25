6c44f2002202e14b2b237788a617f6926a54e9b4
ashiru2010 <ashirubalili@yahoo.com>
2023-03-25 00:51:19 -0700

    task 101

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/amirgambo/alx-low_level_programming/raw/master/0x18-dynamic_libraries/liball.so
+wget -P /tmp/ https://github.com/ashiu2010/alx-low_level_programming/raw/master/0x18-dynamic_libraries/liball.so
 export LD_PRELOAD=/tmp/liball.so
